8e4cc55bff821fbedeede2b725bd8d73c3aa17f2
abrar2139 <mabrarqadri3@mail.com>
2024-04-24 20:09:11 +0500

    night and morning view added

--- script.js
+++ script.js
@@ -1,16 +1,33 @@
 const circle = document.querySelector(".circle");
 const button = document.querySelector("button");
-const body = document.querySelector("body")
+const body = document.querySelector("body");
+const star = document.querySelectorAll(".star")
+const sun = document.querySelector(".sun")
+const cloud = document.querySelector(".content")
+const crsr = document.querySelector(".cursor")
+const container = document.querySelector(".main")
+console.log(star)
 button.textContent = "On";
+
+container.addEventListener("mousemove",function(details) {
+    crsr.style.left=details.x+"px";
+    crsr.style.top=details.y+"px";
+})
 
 let color = 0;
 button.addEventListener("click", function () {
     if (color == 0) {
         circle.style.backgroundColor = "yellow";
-        button.style.backgroundColor= "brown"
-        button.style.color= "yellow"
-        body.style.backgroundColor= "black"
-        button.style.border= "none"
+        cloud.style.display="none"
+        button.style.backgroundColor= "white";
+        for(var i=0;i<Math.min(5,star.length); i++){
+            star[i].style.display="block"
+            star[i].style.transition="all 3s linear"
+        }
+        sun.style.display="block"
+        button.style.color= "black"
+        body.style.background= "linear-gradient(black, DarkSlateGray)"
+        button.style.border= "5px solid white "
         circle.style.boxShadow = "0 0 10px orange, " +
             "0 0 60px orange, " +
             "0 0 200px yellow, " +
@@ -19,14 +36,20 @@ button.addEventListener("click", function () {
         button.textContent = "Off";
         color = 1;
     } else {
-        circle.style.backgroundColor = "transparent";
+        circle.style.backgroundColor = "orange";
+        cloud.style.display="block"
         button.style.backgroundColor= "transparent"
-        body.style.backgroundColor= "white"
-
-        button.style.color= "brown"
-        button.style.border= "5px solid brown "
-        circle.style.boxShadow = "none" 
-
+        body.style.background= "linear-gradient(lightskyblue, white)"
+        for(var i=0;i<Math.min(5,star.length); i++){
+            star[i].style.display="none"
+        }
+        sun.style.display="none"
+        circle.style.boxShadow = "0 0 10px orange, " +
+        "0 0 60px orange, " +
+        "0 0 200px orange, " +
+        "inset 0 0 80px orange";
+        button.style.color= "black"
+        button.style.border= "5px solid black " 
         button.textContent = "On";
         color = 0;
     }
